b2264726a74487ade67abf0fa18547df0ddf7f5c
Bryantw12 <88441225+Bryantw12@users.noreply.github.com>
2022-11-15 10:35:33 -0500

    loops

---
+++ loops.py
@@ -0,0 +1,25 @@
+x = ""
+
+
+# while x != "stop":
+#    #print("hello world")
+#     x = input("type stop to exit")
+
+
+y = 1
+
+# while y <= 10:
+#     print(y)
+
+#     y = y + 1
+
+
+# groceries = ["apple"  , "banana" , "orange"]
+
+# for z in groceries: 
+#     print(z)
+
+
+# this is used to giver a range 1 - 10 
+for i in range (1,11):
+    print(i)
