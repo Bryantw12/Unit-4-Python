981c1f96c8879e75b5d34125e2688271690cb032
Bryantw12 <88441225+Bryantw12@users.noreply.github.com>
2022-11-16 10:56:26 -0500

    average 1.0

---
+++ average.py
@@ -0,0 +1,23 @@
+
+x=int(input("What is your grade?"))
+x = int(x)
+
+list = []  
+list.append = int(input())
+print(list)
+
+
+
+
+
+
+x = [90 , 80 , 60, 0]
+
+sum = 0 
+
+for grade  in x:
+  sum = sum + grade
+
+  print(sum / len(x))  
+
+
