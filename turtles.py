bcf3054d00faf0310d80ba23d493dc25b02eff4a
Bryantw12 <88441225+Bryantw12@users.noreply.github.com>
2022-11-21 09:25:53 -0500

    Dashed Line

---
+++ turtles.py
@@ -0,0 +1,19 @@
+
+from turtle import *
+
+
+penup()
+backward(450)
+while True:
+
+    pendown()
+    forward(300)
+    penup()
+    forward(9)
+    pendown()
+    forward(50)
+    penup()
+    forward(50)
+    pendown()
+
+input()
