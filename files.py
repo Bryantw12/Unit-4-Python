d49f2df8d6d8759f0ca8adb7f2ea78965173c0da
Bryantw12 <88441225+Bryantw12@users.noreply.github.com>
2022-12-06 10:45:05 -0500

    List to text files

--- files.py
+++ files.py
@@ -1,11 +1,35 @@
-my_file = open("Bryant.txt" , "r+")
+# my_file = open("Bryant.txt" , "a")
 
-# print(my_file.readlines())
+# # print(my_file.readlines())
 
-for line in my_file.readlines():
+# my_file.write('im writing from python\
+#   \n')
+
+# my_file.close()
+
+# my_file = open("Bryant.txt")
+
+# for line in my_file.readlines():
+#   print(line, end="")
 
 
-  print(line, end="")
 
-  my_file.writelines(['im writing from python'])
+# with open("todo.txt", "w") as output:
+#     output.write(todos)
+
+while True:
+  my_file = open("todo.txt" , "a",)
+  x = input("What todo would you like to add?")
+  my_file.write(x + "\n")
+  my_file.close()
+
+ 
+  my_file=open("todo.txt","r")
+  print("Your todos are:") 
+  print(my_file.read())
+  my_file.close()
   
+
+
+
+  # todos.append(x)
